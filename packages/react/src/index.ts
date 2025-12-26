152b7b8f4d152440db72748df1c081d29326370b
Youssef-osama33 <yossef2319128@gmail.com>
2025-12-26 19:37:12 +0200

    Create index.ts

---
+++ packages/react/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Palestine Footer - React Package Entry Point
+ */
+
+export { default as PalestineFooter } from './PalestineFooter';
+export type { PalestineFooterProps } from './PalestineFooter';
+
+// Default export
+export { default } from './PalestineFooter';
