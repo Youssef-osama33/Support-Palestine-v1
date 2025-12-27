31e1e3498adcea42f96f69e8ee563ae639346f36
Youssef-osama33 <yossef2319128@gmail.com>
2025-12-27 13:01:44 +0200

    Create index.ts

---
+++ packages/nextjs/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Palestine Footer - Next.js Package Entry Point
+ */
+
+export { default as PalestineFooter } from './PalestineFooter';
+export type { PalestineFooterProps } from './PalestineFooter';
+
+// Default export
+export { default } from './PalestineFooter';
