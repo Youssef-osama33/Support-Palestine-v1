d20ed090883d92935f2e762937b6424b547c4417
Youssef-osama33 <yossef2319128@gmail.com>
2025-12-27 13:10:31 +0200

    Create palestine-footer.module.ts

---
+++ packages/angular/src/Lib/palestine-footer.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PalestineFooterComponent } from './palestine-footer.component';
+
+@NgModule({
+  declarations: [
+    PalestineFooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PalestineFooterComponent
+  ]
+})
+export class PalestineFooterModule { }
